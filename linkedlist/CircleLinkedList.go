af10bf9892ff9898560ad5e71eae315eefa35a00
Mojiayi <woshimojiayi@gmail.com>
2022-10-14 13:09:02 +0800

    循环链表的包名改为linkedlist

--- linkedlist/CircleLinkedList.go
+++ linkedlist/CircleLinkedList.go
@@ -1,4 +1,4 @@
-package timewheel
+package linkedlist
 
 import (
 	"errors"
--- test/CircleLinkedList_test.go
+++ test/CircleLinkedList_test.go
@@ -1,7 +1,7 @@
 package algorithm_test
 
 import (
-	"mojiayi-golang-algorithm/timewheel"
+	"mojiayi-golang-algorithm/linkedlist"
 	"testing"
 )
 
@@ -10,12 +10,12 @@ func TestSimpleTaskNodeLinkedList(t *testing.T) {
 	taskList[0] = "任务0"
 	taskList[1] = "任务1"
 	taskList[2] = "任务2"
-	linkedList := timewheel.CircleLinkedList{}
-	linkedList.AddToHead(&timewheel.Node{ID: 1, Data: taskList})
-	linkedList.AddToTail(&timewheel.Node{ID: 2})
-	linkedList.AddToTail(&timewheel.Node{ID: 3})
-	linkedList.AddToTail(&timewheel.Node{ID: 4})
-	linkedList.AddToTail(&timewheel.Node{ID: 5})
+	linkedList := linkedlist.CircleLinkedList{}
+	linkedList.AddToHead(&linkedlist.Node{ID: 1, Data: taskList})
+	linkedList.AddToTail(&linkedlist.Node{ID: 2})
+	linkedList.AddToTail(&linkedlist.Node{ID: 3})
+	linkedList.AddToTail(&linkedlist.Node{ID: 4})
+	linkedList.AddToTail(&linkedlist.Node{ID: 5})
 	linkedList.Print()
 
 	linkedList.DeleteHead()
