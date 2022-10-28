35cdaf04235c29951a7782001bb530d362a5cf34
Mojiayi <woshimojiayi@gmail.com>
2022-10-28 21:43:54 +0800

    优化循环链表测试用例

--- test/LinkedList_test.go
+++ test/LinkedList_test.go
@@ -25,41 +25,41 @@ func TestCircleLinkedList(t *testing.T) {
 	linkedList.Print()
 
 	linkedList.AddToTail(node2)
-	fmt.Print("添加新的尾节点：")
+	fmt.Print("添加新的尾节点2：")
 	linkedList.Print()
 
 	linkedList.AddToTail(node4)
-	fmt.Print("添加新的尾节点：")
+	fmt.Print("添加新的尾节点4：")
 	linkedList.Print()
 
 	linkedList.Add(node3, node2)
-	fmt.Print("添加新节点到2之后：")
+	fmt.Print("添加新节点3到2之后：")
 	linkedList.Print()
 
 	linkedList.AddToTail(node5)
-	fmt.Print("添加新的尾节点：")
+	fmt.Print("添加新的尾节点5：")
 	linkedList.Print()
 
 	linkedList.Add(node6, node5)
-	fmt.Print("添加新节点到5之后：")
+	fmt.Print("添加新节点6到5之后：")
 	linkedList.Print()
 
 	linkedList.AddToTail(node8)
-	fmt.Print("添加新的尾节点：")
+	fmt.Print("添加新的尾节点8：")
 	linkedList.Print()
 
 	linkedList.Add(node7, node6)
-	fmt.Print("添加新节点到6之后：")
+	fmt.Print("添加新节点7到6之后：")
 	linkedList.Print()
 
 	fmt.Print("完成构建本测试用例的链表：")
 	linkedList.Print()
 
-	fmt.Print("删除链表头节点：")
+	fmt.Print("删除链表头节点1：")
 	linkedList.DeleteHead()
 	linkedList.Print()
 
-	fmt.Print("删除链表尾节点：")
+	fmt.Print("删除链表尾节点8：")
 	linkedList.DeleteTail()
 	linkedList.Print()
 
