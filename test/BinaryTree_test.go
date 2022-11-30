d7b2b73292c8cf0b7dd7031864da0d6222c064c4
Mojiayi <woshimojiayi@gmail.com>
2022-11-30 23:30:54 +0800

    更换二叉树的测试数据

--- test/BinaryTree_test.go
+++ test/BinaryTree_test.go
@@ -9,7 +9,7 @@ import (
 func TestPreorderTraversal(t *testing.T) {
 	var binaryTree = buildBinaryTree()
 
-	var exceptedResult []string = []string{"m", "k", "h", "a", "b", "i", "c", "d", "l", "j", "e", "f"}
+	var exceptedResult []string = []string{"a", "b", "e", "h", "k", "l", "i", "m", "j", "f", "c", "d", "n", "g"}
 	var index = 0
 	binaryTree.PreorderTraversal(func(args interface{}) {
 		if args.(string) != exceptedResult[index] {
@@ -23,7 +23,7 @@ func TestPreorderTraversal(t *testing.T) {
 func TestMiddleOrderTraversal(t *testing.T) {
 	var binaryTree = buildBinaryTree()
 
-	var exceptedResult []string = []string{"a", "h", "b", "k", "c", "i", "d", "m", "l", "e", "j", "f"}
+	var exceptedResult []string = []string{"k", "h", "l", "e", "m", "i", "j", "b", "f", "a", "d", "n", "c", "g"}
 	var index = 0
 	binaryTree.MiddleOrderTraversal(func(args interface{}) {
 		if args.(string) != exceptedResult[index] {
@@ -37,7 +37,7 @@ func TestMiddleOrderTraversal(t *testing.T) {
 func TestPostOrderTraversal(t *testing.T) {
 	var binaryTree = buildBinaryTree()
 
-	var exceptedResult []string = []string{"a", "b", "h", "c", "d", "i", "k", "e", "f", "j", "l", "m"}
+	var exceptedResult []string = []string{"k", "l", "h", "m", "j", "i", "e", "f", "b", "n", "d", "g", "c", "a"}
 	var index = 0
 	binaryTree.PostorderTraversal(func(args interface{}) {
 		if args.(string) != exceptedResult[index] {
@@ -51,18 +51,23 @@ func TestPostOrderTraversal(t *testing.T) {
 func buildBinaryTree() *tree.BinaryTree {
 	var binaryTree = new(tree.BinaryTree)
 
-	binaryTree.Root = &tree.BinaryTreeNode{ID: 0, Data: "m"}
-	var leftNode, _ = binaryTree.Root.AddToLeft("k")
-	var rightNode, _ = leftNode.AddToRight("i")
-	leftNode, _ = leftNode.AddToLeft("h")
-	leftNode.AddToLeft("a")
-	leftNode.AddToRight("b")
-	rightNode.AddToLeft("c")
-	rightNode.AddToRight("d")
-	rightNode, _ = binaryTree.Root.AddToRight("l")
-	rightNode, _ = rightNode.AddToRight("j")
-	rightNode.AddToLeft("e")
-	rightNode.AddToRight("f")
+	binaryTree.Root = &tree.BinaryTreeNode{ID: 0, Data: "a"}
+	nodeB, _ := binaryTree.Root.AddToLeft("b")
+	nodeC, _ := binaryTree.Root.AddToRight("c")
+
+	nodeE, _ := nodeB.AddToLeft("e")
+	nodeB.AddToRight("f")
+	nodeD, _ := nodeC.AddToLeft("d")
+	nodeC.AddToRight("g")
+
+	nodeH, _ := nodeE.AddToLeft("h")
+	nodeI, _ := nodeE.AddToRight("i")
+	nodeD.AddToRight("n")
+
+	nodeH.AddToLeft("k")
+	nodeH.AddToRight("l")
+	nodeI.AddToLeft("m")
+	nodeI.AddToRight("j")
 
 	return binaryTree
 }
